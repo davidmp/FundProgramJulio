f6ab425754ead83e14f09c435c485a3be4e19ee0
davidmp <mamanipari@gmail.com>
2019-07-16 09:37:41 -0500

    Algoritmos Recursivos

--- AlgoritmosRepasoPython/upeu/OrdenacionBusqueda.py
+++ AlgoritmosRepasoPython/upeu/OrdenacionBusqueda.py
@@ -2,7 +2,7 @@ from ctypes import py_object
 
 
 class OrdenacionYBusqueda():
-    def insercionDirecta(self, vertor):
+    def insercionDirecta(self, vector):
         aux,posicion=0,0
         for i in range(1, len(vector)):
             aux=vector[i]
@@ -14,6 +14,20 @@ class OrdenacionYBusqueda():
             vector[posicion]=aux
             print("Proceso for:", vector)
         return vector
+
+    def insercionDirectaRecur(self, vector, i):
+        aux, posicion = 0, 0
+        if(i<len(vector)):
+            aux=vector[i]
+            posicion=i
+            while(posicion>0 and aux<vector[posicion-1]):
+                vector[posicion]=vector[posicion-1]
+                posicion=posicion-1
+                print("Proceso While:",vector)
+            vector[posicion]=aux
+            vector=self.insercionDirectaRecur(vector,i+1)
+        return vector
+
     def ordenacionSortBurbuja(self, vector):
         posicion, aux=0,0
         while(posicion<len(vector)):
@@ -28,10 +42,21 @@ class OrdenacionYBusqueda():
                 print(vector)
         return vector
 
-
+    def ordenacionSortBurbujaRecur(self,vector, posicion):
+        if(posicion<len(vector)):
+            if(posicion==0 or vector[posicion]>=vector[posicion-1]):
+                posicion=posicion+1
+                self.ordenacionSortBurbujaRecur(vector, posicion)
+            else:
+                aux=vector[posicion-1]
+                vector[posicion - 1]=vector[posicion]
+                vector[posicion]=aux
+                posicion=posicion-1
+                self.ordenacionSortBurbujaRecur(vector,posicion)
+        return vector
 
 obj=OrdenacionYBusqueda()
 vector=[4,10,1,8,3]
 print("Antes:", vector)
 #print("Ordenado:",obj.insercionDirecta(vector))
-print("Ordenar Burbuja", obj.ordenacionSortBurbuja(vector))
+print("Ordenar Recursiva", obj.ordenacionSortBurbujaRecur(vector,0))
