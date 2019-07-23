91bd9d403fa069710dfb60932730fb16467beecc
davidmp <mamanipari@gmail.com>
2019-07-23 12:28:01 -0500

    Ejemplo Matrices transforma

--- AlgoritmosRepasoPython/upeu/Matriz.py
+++ AlgoritmosRepasoPython/upeu/Matriz.py
@@ -13,5 +13,44 @@ class OperacionMatrices():
         print("Suma total de valores de la Matriz :", sumaTotal)
         print("Suma total de valores pares de la Matriz es:", sumPares)
 
+    def transformada04(self, dimension, numInicio):
+        M=[[0] * dimension for i in range(dimension)]
+        for x in range(0, dimension):
+            for y in range(0, dimension):
+                if(y<dimension-x):
+                    if (x+y)%2==0:
+                        M[x][y]=numInicio+((x+y)*(x+y+1)/2)+x
+                    else:
+                        M[x][y]=numInicio+((x+y)*(x+y+1)/2)+y
+                else:
+                    M[x][y]=""
+        return M
+
+    def transformada17(self, dimension, numInicio):
+        M = [[0] * dimension for i in range(dimension)]
+        contadorX, contadorY, contGe=0,dimension-1, 0
+        while (contadorY >= 0):
+            print("Valor Y:", contadorY, " contGe:", contGe, "Valor X", contadorX, "numInicio:", numInicio)
+            while(contadorX<dimension-contGe):
+                M[contadorX][contadorY]=numInicio
+                numInicio = numInicio + 1
+                contadorX=contadorX+1
+                print("Valor Y:", contadorY, " contGe:", contGe, "Valor X", contadorX, "numInicio:", numInicio)
+            contadorY=contadorY-1
+            contGe=contGe+1
+            contadorX=0
+        return M
+
+    def imprimirMatriz(self, matriz):
+        for x in range(0, len(matriz)):
+            for y in range(0,len(matriz[0])):
+                print(matriz[x][y],"\t",end='')
+            print("\n")
+
 obj=OperacionMatrices()
 obj.sumaValoresMatriz()
+dimension=int(input("Ingrese la dimesion de la Matriz:"))
+numInicio=int(input("Ingrese el numero de inicio:"))
+
+#obj.imprimirMatriz(obj.transformada04(dimension, numInicio))
+obj.imprimirMatriz(obj.transformada17(dimension, numInicio))
